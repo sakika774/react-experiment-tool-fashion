80389acc648f84f592d7fdba78e1cec2d9a5eff4
sakika774 <sakikalab74@gmail.com>
2025-10-28 17:36:18 +0900

    Add image data definition

---
+++ frontend/src/data/images.ts
@@ -0,0 +1,17 @@
+export const imageSets = [
+  {
+    id: 1,
+    condition: "A",
+    images: ["image1a.jpg", "image1b.jpg", "image1c.jpg"],
+  },
+  {
+    id: 2,
+    condition: "B",
+    images: ["image2a.jpg", "image2b.jpg", "image2c.jpg"],
+  },
+  {
+    id: 3,
+    condition: "C",
+    images: ["image3a.jpg", "image3b.jpg", "image3c.jpg"],
+  },
+];
